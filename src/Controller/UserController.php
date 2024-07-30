e022f39f639b1cb458bd8e03db1e9eab32609a1b
Maxime Verriest <maxime.verriest@quadra-informatique.fr>
2024-07-30 10:49:18 +0200

    correctif user route

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -13,7 +13,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route('user')]
+#[Route('api/user')]
 class UserController extends AbstractController
 {
     #[Route('/', name: 'app_user_index', methods: ['GET'])]
